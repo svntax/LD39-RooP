c84f8d142d917f4fd4c47d5de068fe871714a833
TheBlueGamer55 <endless.answers@gmail.com>
2017-07-29 18:28:58 -0700

    Add WIP grid-tile system with example path navigation

---
+++ Scripts/grid_tile.gd
@@ -0,0 +1,61 @@
+
+extends Node2D
+
+##Tile types:##
+#0 = horizontal (left-right)
+#1 = vertical (up-down)
+#2 = right-down corner
+#3 = left-down corner
+#4 = up-left corner
+#5 = up-right corner
+
+#The points where a spark can enter or exit from
+var UP = 0
+var LEFT = 1
+var RIGHT = 2
+var DOWN = 3
+
+var type
+var exit1
+var exit2
+var sparkInTile #If there is currently a spark in this tile
+
+func _ready():
+	sparkInTile = false
+
+func getType():
+	return type
+
+func setType(type):
+	self.type = type
+	var tileSprite = self.find_node("Sprite")
+	#First 6 basic tiles are in the first row of the tileset
+	if(type >= 0 or type <= 5):
+		tileSprite.set_region_rect(Rect2(type*16, 0, 16, 16))
+	if(type == 0): #left-right horizontal tile
+		exit1 = LEFT
+		exit2 = RIGHT
+	elif(type == 1): #up-down vertical tile
+		exit1 = UP
+		exit2 = DOWN
+	elif(type == 2): #right-down corner tile
+		exit1 = RIGHT
+		exit2 = DOWN
+	elif(type == 3): #left-down corner tile
+		exit1 = LEFT
+		exit2 = DOWN
+	elif(type == 4): #up-left corner tile
+		exit1 = UP
+		exit2 = LEFT
+	elif(type == 5): #up-right corner tile
+		exit1 = UP
+		exit2 = RIGHT
+
+func getExit1():
+	return exit1
+
+func getExit2():
+	return exit2
+
+func isSparkInTile():
+	return sparkInTile
---
+++ Scripts/powergrid.gd
@@ -0,0 +1,122 @@
+
+extends Node2D
+
+var GRID_WIDTH = 16
+var GRID_HEIGHT = 12
+
+var sparkScene = load("res://Scenes/spark.tscn")
+var gridTileScene = load("res://Scenes/grid_tile.tscn")
+var grid #2D array of tiles represented by their type
+var objectGrid #2D array of tile objects
+
+##Tile types:##
+#0 = horizontal (left-right)
+#1 = vertical (up-down)
+#2 = right-down corner
+#3 = left-down corner
+#4 = up-left corner
+#5 = up-right corner
+
+#The 4 exits possible for each tile
+var UP = 0
+var LEFT = 1
+var RIGHT = 2
+var DOWN = 3
+
+#test function
+func testNavigate(startX, startY):
+	var startDir = LEFT #spark will enter from the left
+	testNavigateHelper(startX, startY, startDir)
+
+func testNavigateHelper(x, y, entryDir):
+	#Stay within  bounds
+	if(x < 0 or x > GRID_WIDTH - 1 or y < 0 or y > GRID_HEIGHT - 1):
+		return
+	var tileObj = getTileAt(x, y)
+	#Make sure a tile object exists at (x, y)
+	if(tileObj == null):
+		return
+	var exit1 = tileObj.getExit1()
+	var exit2 = tileObj.getExit2()
+	#Check if spark can enter the tile
+	if(entryDir == exit1 or entryDir == exit2):
+		#Test - visually mark the tile visited with a spark
+		var spark = sparkScene.instance()
+		spark.set_pos(tileObj.get_pos() + Vector2(8, 8))
+		self.add_child(spark)
+		var nextEntryDir = -1
+		var currentExit = -1
+		#Get the next direction the spark will enter from
+		if(entryDir == exit1):
+			currentExit = exit2
+		elif(entryDir == exit2):
+			currentExit = exit1
+		#Recursively check the next tile
+		if(currentExit == UP):
+			testNavigateHelper(x, y - 1, DOWN)
+		elif(currentExit == LEFT):
+			testNavigateHelper(x - 1, y, RIGHT)
+		elif(currentExit == RIGHT):
+			testNavigateHelper(x + 1, y, LEFT)
+		elif(currentExit == DOWN):
+			testNavigateHelper(x, y + 1, UP)
+	else:
+		#Spark fizzles out
+		pass
+
+func _ready():
+	randomize() #Change the seed for any random operations
+	#Create 2D array for grid
+	grid = []
+	objectGrid = []
+	for x in range(GRID_WIDTH):
+		grid.append([])
+		for y in range(GRID_HEIGHT):
+			grid[x].append(randi() % 6) #Random tiles for now
+	#Create a 2D array for the actual tile objects
+	for x in range(GRID_WIDTH):
+		objectGrid.append([])
+		for y in range(GRID_HEIGHT):
+			objectGrid[x].append(null)
+	createTilesFromGrid(grid)
+	set_process_input(true)
+
+func _input(event):
+	if(event.type == InputEvent.MOUSE_BUTTON):
+		if(event.button_index == BUTTON_LEFT && event.pressed):
+			#Test - click on a tile, then sparks will form on the path
+			#starting from the left side of the clicked tile
+			var mouseX = event.pos.x
+			var mouseY = event.pos.y
+			var mouseTileX = floor(mouseX / 32)
+			var mouseTileY = floor(mouseY / 32)
+			testNavigate(mouseTileX, mouseTileY)
+
+#After the 2D array of numbers for the tiles is generated,
+#spawn the tile objects
+func createTilesFromGrid(grid):
+	for x in range(GRID_WIDTH):
+		for y in range(GRID_HEIGHT):
+			var tileType = grid[x][y]
+			var tileObj = gridTileScene.instance()
+			tileObj.set_pos(Vector2(x*16, y*16))
+			tileObj.setType(tileType)
+			self.add_child(tileObj)
+			objectGrid[x][y] = tileObj
+
+func getTileTypeAt(x, y):
+	return grid[x][y]
+
+func getTileAt(x, y):
+	return objectGrid[x][y]
+
+#Debug function - used to print an exit direction as a string
+func exitAsString(exit):
+	if(exit == LEFT):
+		return "LEFT"
+	elif(exit == RIGHT):
+		return "RIGHT"
+	elif(exit == UP):
+		return "UP"
+	elif(exit == DOWN):
+		return "DOWN"
